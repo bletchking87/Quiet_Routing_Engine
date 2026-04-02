b5e22e83f73b7f8c447eeea1981cd6187aba3844
bletchking87 <rapple05@gmail.com>
2026-04-02 14:52:03 +0100

    Added more state management features and re-ordered.

--- initial_mapping.py
+++ initial_mapping.py
@@ -13,34 +13,42 @@ ox.settings.requests_timeout = 300
 client = anthropic.Anthropic()
 
 # ------------------------ Streamlit Session State Initialisation ------------------------
-if 'route_fast' not in st.session_state:
-    st.session_state.route_fast = None
+def init_session_state():#Turned into a function as had too many variables to initialise. 
+    defaults = {
+        'route_fast': None,
+        'orig': None,
+        'dest': None,
+        'last_k_label': None,
+        'summary': None,
+        'last_route': None,
+        'G': None,
+        'edges': None,
+        'edges_projected': None,
+        'noise_normalised': None,
+        'route_fast_edges': None,
+    }
+    for key, value in defaults.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
 
-if 'orig' not in st.session_state:
-    st.session_state.orig = None
-
-if 'dest' not in st.session_state:
-    st.session_state.dest = None
-
-if 'last_k_label' not in st.session_state:
-    st.session_state.last_k_label = None
-
-if 'summary' not in st.session_state:
-    st.session_state.summary = None
-
-if 'last_route' not in st.session_state:
-    st.session_state.last_route = None
+init_session_state()
 
 
 # ------------------------ Extracting the 'Walk' network for BCN ------------------------
 gpkg_path = "noise_data_2017.gpkg"
-@st.cache_data #Caching the graph to speed up subsequent runs, especially during development.
-def load_graph(address, dist=3000):
-    print("Scanning Barcelona...")
-    G = ox.graph_from_address(address, dist, network_type="walk")
-    _, edges = ox.graph_to_gdfs(G) #leaving nodes (the underscore) as we are working with roads, not intersections.
+
+# ------------------------------ USER INPUT FOR START AND END POINTS ------------------------------
+start_input = st.sidebar.text_input("Enter your starting point (e.g., 'Plaça de Catalunya, Barcelona'):", placeholder="Parc Joan Miró, Barcelona")
+end_input = st.sidebar.text_input("Enter your destination (e.g., 'Sagrada Família, Barcelona'):", placeholder="Sagrada Família, Barcelona")
+
+
+bbox = ("north", "south", "east", "west")
+@st.cache_data
+def load_graph(bbox):
+    G = ox.graph_from_bbox(bbox, network_type="walk")
+    _, edges = ox.graph_to_gdfs(G) #leaving nodes blank (hence the underscore) as we are working with roads, not intersections.
     return G, edges
-G, edges = load_graph("Barcelona, Spain")
+
 
 #FETCHING TIME IN BARCELONA - HARDCODING LOCAL TIME TO AVOID ISSUES WITH TIMEZONE CONFIGURATION IN DIFFERENT ENVIRONMENTS. 
 #FOR A MUlTICITY APPLICATION, THIS WOULD NEED TO BE CONFIGURED TO FETCH LOCAL TIME BASED ON THE CITY IN QUESTION.
@@ -65,11 +73,11 @@ noise_column = get_noise_column() #Calling function outside the caching that com
 # -------------------- STREAMLIT SLIDER FOR NOISE SENSITIVITY (k) --------------------
 k_label = st.sidebar.select_slider(    #Tuning parameter to adjust influence of noise on the overall cost. 
         'Walking preference',
-        options=['Fastest', 'Balanced', 'Quiet', 'Serene'],
+        options=['Efficient', 'Balanced', 'Quiet', 'Serene'],
         value='Balanced'    #Hardcoded starting point
     )
 mapping = {
-        'Fastest': 0.5,
+        'Efficient': 0.5,
         'Balanced': 1.5,
         'Quiet': 3,
         'Serene': 5
@@ -112,55 +120,62 @@ def map_data_join(_edges, gpkg_path, noise_column):     #Have put "_edges" so th
 # ---------------------- Applying Noise Constraints to the Edges --------------------
 
 def set_noise_constraints(noise_normalised, edges_projected, k):
-    return edges_projected['length'] + (noise_normalised ** k) * 500 #decibels are logarithmic, so we apply the exponent to reflect the non-linear increase in perceived noise. 
+    penalty = ((1 + noise_normalised) ** k) - 1 #decibels are logarithmic, so we apply the exponent to reflect the non-linear increase in perceived noise. 
+    return edges_projected['length'] * (1+penalty) #Adding 1 to ensure the cost is always positive
 
-noise_normalised, edges_projected = map_data_join(edges, gpkg_path, noise_column) #Joining the noise data to the edges, and normalising it. This is cached to speed up subsequent runs, as this is the most computationally expensive part of the process.
-weighted_cost = set_noise_constraints(noise_normalised, edges_projected, k) #Applying the noise constraints to the edges, with the selected k value.
 
-# ------------------------------------ Routing Comparison ------------------------------------
-edges['weighted_cost'] = weighted_cost  #Converting back to a Series of values to push back to the graph. 
-weights_dict = edges['weighted_cost'].to_dict() #Precautionary step to ensure weights are formatted such that they can be pushed back easily to graph.
+if st.session_state.noise_normalised is not None:
+    weighted_cost = set_noise_constraints(st.session_state.noise_normalised, st.session_state.edges_projected, k) #Applying the noise constraints to the edges, with the selected k value.
+    # ------------------------------------ Routing Comparison ------------------------------------
+    st.session_state.edges['weighted_cost'] = weighted_cost  #Converting back to a Series of values to push back to the graph. 
+    weights_dict = st.session_state.edges['weighted_cost'].to_dict() #Precautionary step to ensure weights are formatted such that they can be pushed back easily to graph.
+    nx.set_edge_attributes(st.session_state.G, weights_dict, 'weighted_cost') # Push scores back to the graph
 
-nx.set_edge_attributes(G, weights_dict, 'weighted_cost') # Push scores back to the graph
-
-# ------------------------------ USER INPUT FOR START AND END POINTS ------------------------------
-
-start_input = st.sidebar.text_input("Enter your starting point (e.g., 'Plaça de Catalunya, Barcelona'):", placeholder="Parc Joan Miró, Barcelona")
-end_input = st.sidebar.text_input("Enter your destination (e.g., 'Sagrada Família, Barcelona'):", placeholder="Sagrada Família, Barcelona")
-
-if not start_input or not end_input:
-    st.error("Please enter a starting point and a destination.")
-    st.stop() #Stop the app if either input is missing, to avoid errors in geocoding.
-try: 
-    start_point = ox.geocoder.geocode(start_input)
-except Exception as e:
-    st.error(f"Couldn't geocode starting point: {e}")
-    st.stop()
-if start_point is None:
-    st.error("Couldn't geocode starting point. Please check your input and try again.")
-    st.stop()
-
-try:
-    end_point = ox.geocoder.geocode(end_input)
-except Exception as e:
-    st.error(f"Couldn't geocode destination: {e}")
-    st.stop()
-if end_point is None:
-    st.error("Couldn't geocode destination. Please check your input and try again.")
-    st.stop()
 
 # ------------ ROUTE MAPPING, VISUALISATION AND SUMMARY ------------- #Button block is separated to allow for faster iterations on routing. 
 if st.sidebar.button("Find route"): 
     with st.spinner("Calculating routes..."):
-        st.session_state.orig = ox.distance.nearest_nodes(G, X=start_point[1], Y=start_point[0])    
-        st.session_state.dest = ox.distance.nearest_nodes(G, X=end_point[1], Y=end_point[0])
-        st.session_state.route_fast = ox.shortest_path(G, st.session_state.orig, st.session_state.dest, weight='length')
-        st.session_state.route_fast_edges = ox.routing.route_to_gdf(G, st.session_state.route_fast)
+        if not start_input or not end_input:
+            st.error("Please enter a starting point and a destination.")
+            st.stop() #Stop the app if either input is missing, to avoid errors in geocoding.
+        try: 
+            start_point = ox.geocoder.geocode(start_input)
+        except Exception as e:
+            st.error(f"Couldn't geocode starting point: {e}")
+            st.stop()
+        if start_point is None:
+            st.error("Couldn't geocode starting point. Please check your input and try again.")
+            st.stop()
+        try:
+            end_point = ox.geocoder.geocode(end_input)
+        except Exception as e:
+            st.error(f"Couldn't geocode destination: {e}")
+            st.stop()
+        if end_point is None:
+            st.error("Couldn't geocode destination. Please check your input and try again.")
+            st.stop()
+
         
+        north = max(start_point[0], end_point[0]) + 0.01 #0.01 degrees is roughly 1km, allowing for ample expansion of the map for longer routes. 
+        south = min(start_point[0], end_point[0]) - 0.01
+        east = max(start_point[1], end_point[1]) + 0.01
+        west = min(start_point[1], end_point[1]) - 0.01
+
+        st.info("Loading graph data for the specified area...")#Better UX
+        bbox = (north, south, east, west)
+        G, edges = load_graph(bbox) #Reloading graph with a bounding box that encompasses the start and end points, to speed up routing calculations. This is cached to speed up subsequent runs with the same bounding box.
+        st.session_state.G = G
+        st.session_state.edges = edges
+        st.session_state.noise_normalised, st.session_state.edges_projected = map_data_join(edges, gpkg_path, noise_column) #Joining normalised noise data to the edges
+
+        st.session_state.orig = ox.distance.nearest_nodes(st.session_state.G, X=start_point[1], Y=start_point[0])    
+        st.session_state.dest = ox.distance.nearest_nodes(st.session_state.G, X=end_point[1], Y=end_point[0])
+        st.session_state.route_fast = ox.shortest_path(st.session_state.G, st.session_state.orig, st.session_state.dest, weight='length')
+        st.session_state.route_fast_edges = ox.routing.route_to_gdf(st.session_state.G, st.session_state.route_fast)
 
 if st.session_state.orig is not None:
-    route_quiet = ox.shortest_path(G, st.session_state.orig, st.session_state.dest, weight='weighted_cost')
-    route_quiet_edges = ox.routing.route_to_gdf(G, route_quiet)
+    route_quiet = ox.shortest_path(st.session_state.G, st.session_state.orig, st.session_state.dest, weight='weighted_cost')
+    route_quiet_edges = ox.routing.route_to_gdf(st.session_state.G, route_quiet)
     
     
     #Finding which roads are in the quiet but not in the fast route.
@@ -168,8 +183,8 @@ if st.session_state.orig is not None:
     fast_road_names = st.session_state.route_fast_edges['name'].explode().unique().tolist()
     main_roads_avoided = [road for road in fast_road_names if road not in quiet_road_names and road is not None]
     
-    fast_noise = edges_projected.loc[st.session_state.route_fast_edges.index, 'noise_values'].mean().round() 
-    quiet_noise = edges_projected.loc[route_quiet_edges.index, 'noise_values'].mean().round()
+    fast_noise = st.session_state.edges_projected.loc[st.session_state.route_fast_edges.index, 'noise_values'].mean().round() 
+    quiet_noise = st.session_state.edges_projected.loc[route_quiet_edges.index, 'noise_values'].mean().round()
 
 
     len_fast = st.session_state.route_fast_edges['length'].sum()
@@ -182,7 +197,7 @@ if st.session_state.orig is not None:
     st.metric(label=f"Quiet Route, {k_label} mode", value=f"{len_quiet/1000:.1f} km. Estimated time: {int(quiet_time)} minutes.")
  
 # -------------- Plotting routes using OSMNX's built-in plotting function ------------------
-    fig, ax = ox.plot_graph_routes(G, [st.session_state.route_fast, route_quiet], 
+    fig, ax = ox.plot_graph_routes(st.session_state.G, [st.session_state.route_fast, route_quiet], 
                                 route_colors=['r', 'g'], 
                                 route_linewidth=4, node_size=0)
     st.pyplot(fig)
@@ -197,12 +212,12 @@ if st.session_state.orig is not None:
         response = client.messages.create(
             model="claude-sonnet-4-5",
             max_tokens=1024,
-            system=f"""You are a helpful walking assistant that provides a concise summary of the features of a quietness-optimised route through barcelona, 
+            system=f"""You are a helpful walking assistant that provides a concise summary of the features of a quietness-optimised route through Barcelona, 
             in contrast to the fastest route. The user has chosen {k_label} as their mode. 
             Focus on the noise levels, roads avoided, and time difference in your summary. 
             Note that decibels are logarithmic — their increase and decrease is not linear: 
             Even a small difference in dB can have a significant impact on perceived noise. 
-            If the difference between fast and quiet route noise is less than 2dB, 
+            If {k_label} is "Serene" and the difference between fast and quiet route noise is less than 2dB, 
             note that this area of Barcelona is uniformly loud and even the quietest available route has limited noise reduction.
             Avoid giving specific percentages and instead use qualitative language like "noticeably quieter" or "significantly reduced".
             Reflect this in your summary so the user understands the real impact of the noise difference.
